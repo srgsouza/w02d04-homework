329f7d22a90a420ca34707d18d012a36e1ab9817
Sergio Souza <emailsouza@gmail.com>
2018-06-22 13:48:07 -0600

    Commit 0 - All my files are linked and loaded

---
+++ js/app.js
@@ -0,0 +1,24 @@
+//   JQuery Homework
+
+// Setup - Get Ready to Make So Much Magic
+// Make a folder inside your homework folder for today called hogwarts.
+// cd into the hogwarts folder
+// touch index.html
+// mkdir css
+// touch css / style.css
+// mkdir js
+// touch js / app.js
+// 
+
+$(document).ready(function () {
+  if (typeof jQuery == 'undefined') {
+    console.log('oops! I still have to link my jQuery properly!');
+  } else {
+    console.log('I did it! I linked jQuery and this js file!')
+  };
+
+
+
+
+
+});
